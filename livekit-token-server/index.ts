4e27bd502a5a0f5f2ddb4e0a38190eedeaf881ff
cp-20 <n.naoki.1587@gmail.com>
2025-03-06 15:10:17 +0900

    room / userrname をクエリパラメータとして受け取れるように

--- livekit-token-server/index.ts
+++ livekit-token-server/index.ts
@@ -1,23 +1,30 @@
+import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
-import { AccessToken } from "livekit-server-sdk";
+import { AccessToken, Room } from "livekit-server-sdk";
+import { z } from "zod";
 
-const createToken = async () => {
-  const roomName = "quickstart-room";
-  const participantName = "quickstart-username";
-
+const createToken = async (room: string, username: string) => {
   const at = new AccessToken(
     process.env.LIVEKIT_API_KEY,
     process.env.LIVEKIT_API_SECRET,
-    { identity: participantName, ttl: "10m" },
+    { identity: username, ttl: "10m" },
   );
-  at.addGrant({ roomJoin: true, room: roomName });
+  at.addGrant({ roomJoin: true, room });
   return await at.toJwt();
 };
 
 const app = new Hono();
 const port = Number(process.env.PORT) || 3000;
 
-app.get("/getToken", async (c) => c.text(await createToken()));
+const getTokenQuerySchema = z.object({
+  room: z.string(),
+  username: z.string(),
+});
+
+app.get("/getToken", zValidator("query", getTokenQuerySchema), async (c) => {
+  const { room, username } = c.req.valid("query");
+  return c.text(await createToken(room, username));
+});
 
 export default {
   fetch: app.fetch,
